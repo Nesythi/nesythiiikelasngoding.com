22aa8a0182e1a6bf1a0b600a1921a91c187f3527
Nesythi <nesythiiimusidah@gmail.com>
2026-06-11 16:03:20 +0700

    Create Pseudocode.c

---
+++ Kumpulan Tugas/Pseudocode.c
@@ -0,0 +1,14 @@
+PSEUDOCODE
+
+Algoritma Menentukan Kelulusan
+(Penjelasan Singkat Mengenai Kelulusan
+Kamus:
+n: Intenger
+
+Deskripsi:
+Read (nilai)
+If nilai > 50 then
+Write (“ anda lulus”)
+Else
+Write (“ anda gagal”)
+endif
